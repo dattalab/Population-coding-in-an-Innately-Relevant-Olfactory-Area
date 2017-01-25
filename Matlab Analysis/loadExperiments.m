46f7b43495affa9cf7cc1cb17001078d97b3d49f
Giuliano Iurilli <Giuliano@Giulianos-MacBook-Pro-2.local>
2017-01-25 13:38:58 -0500

    remove lines that are not used

--- Matlab Analysis/loadExperiments.m
+++ Matlab Analysis/loadExperiments.m
@@ -10,22 +10,3 @@ coaNM = load('plCoA_natMix_2.mat');
 coaCS2 = load('plCoA_CS2_2.mat');
 pcxCS2 = load('aPCx_CS2_2.mat');
 
-
-pcxAA_1 = load('aPCx_AA_1.mat');
-pcx15_1 = load('aPCx_15_1.mat');
-coa15_1 = load('plCoA_15_1.mat');
-coaAA_1 = load('plCoA_AA_1.mat');
-pcxNM_1 = load('aPCx_natMix_1.mat');
-coaNM_1 = load('plCoA_natMix_1.mat');
-coaCS2_1 = load('plCoA_CS2_1.mat');
-pcxCS2_1 = load('aPCx_CS2_1.mat');
-
-
-pcxAA_3 = load('aPCx_AA_3.mat');
-pcx15_3 = load('aPCx_15_3.mat');
-coa15_3 = load('plCoA_15_3.mat');
-coaAA_3 = load('plCoA_AA_3.mat');
-pcxNM_3 = load('aPCx_natMix_3.mat');
-coaNM_3 = load('plCoA_natMix_3.mat');
-coaCS2_3 = load('plCoA_CS2_3.mat');
-pcxCS2_3 = load('aPCx_CS2_3.mat');
